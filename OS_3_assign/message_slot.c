966790ca541a952c56875899046843121c6ca01f
adam <adam@adam.adam>
2022-04-12 08:14:25 +0300

    infrastructure created.

---
+++ OS_3_assign/message_slot.c
@@ -0,0 +1,46 @@
+
+
+
+
+
+// Defining __KERNEL__ and MODULE allows us
+// to access kernel-level code.
+// --------------------------------------------------------
+
+// We are part of the kernel
+#undef __KERNEL__
+#define __KERNEL__
+
+
+#undef MODULE
+#define MODULE
+
+// included for all kernel modules
+#include <linux/module.h>
+// included for __init and __exit macros
+#include <linux/init.h>
+
+
+
+
+
+
+
+//--- loader ---------------------------------------------
+static __init int module_init(void)
+{
+  printk("Initializing the message slot module!\n");
+  return 0;
+}
+
+//--- unloader -------------------------------------------
+static void __exit module_cleanup(void)
+{
+  printk("Destructing the message slot module!\n");
+}
+
+//--------------------------------------------------------
+module_init(panic_init);
+module_exit(panic_cleanup);
+
+//=================== END OF FILE ========================
---
+++ comp.sh
@@ -0,0 +1,2 @@
+gcc -O3 -Wall -std=c11 message_sender.c -o sender 
+gcc -O3 -Wall -std=c11 message_reader.c -o reader
