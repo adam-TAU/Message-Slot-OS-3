fbcfb50a62bc551bf442e871ccf0425517c72638
adam <adam@adam.adam>
2022-04-13 08:43:21 +0300

    finished building the main implementation for message_reader/sender. Need to add error handling and will be done. -> debug

--- OS_3_assign/message_reader.c
+++ OS_3_assign/message_reader.c
@@ -1,7 +1,10 @@
 #include "message_slot.h"
 
+#include <fcntl.h>      /* open */ 
+#include <unistd.h>     /* exit */
+#include <sys/ioctl.h>  /* ioctl */
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 
 
 
@@ -16,16 +19,26 @@ int main(int args, char* argv[]) {
 		exit(1);
 	}
 	
-	/* Open the device file descriptor */
-	fd = open();
+	// parsing the data
+	char* chr_dev_path = argv[1]);
+	int channel_id = atoi(argv[2]);
 	
-	/* Read the message, and save the amount of bytes read into the variable 'count' */
-	count = read();
+	// open the device file
+	fd = open(chr_dev_path, O_RDONLY);
+	
+	// Read the message, and save the amount of bytes read into the variable 'count'
+	char message[BUF_LEN];
+	count = read(fd, message, BUF_LEN);
+	
+	// close the device file
+	close(fd);
+	
+	// verify that the return value of the read syscall indicates success
 	if (count < 0) {
 		perror("Error with reading the message from the message channel");
 		exit(1);
 	}
 	
-	/* Print the message */
+	// Print the message
 	write(STDOUT_FILENO, the_message, count);
 }
--- OS_3_assign/message_sender.c
+++ OS_3_assign/message_sender.c
@@ -1,7 +1,11 @@
 #include "message_slot.h"
 
-
+#include <fcntl.h>      /* open */ 
+#include <unistd.h>     /* exit */
+#include <sys/ioctl.h>  /* ioctl */
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
 
@@ -12,4 +16,25 @@ int main(int args, char* argv[]) {
 		perror("Incorrect amount of arguments");
 		exit(1);
 	}
+	
+	// parsing the data
+	char* chr_dev_path = argv[1]);
+	int channel_id = atoi(argv[2]);
+	
+	// removing the terminating NULL character from the end of the string
+	unsigned int message_length = strlen(argv[3]);
+	char clean_message[message_length];
+	memcpy(clean_message, argv[3], message_length);
+	
+	// open the device file
+	int fd = open(chr_dev_path, O_WRONLY);
+	
+	// set the channel
+	ioctl(fd, MSG_SLOT_CHANNEL, channel_id);
+	
+	// write the clean message into the channel buffer
+	write(fd, clean_message, message_length);
+	
+	// close the device file
+	close(fd);
 }
